489b8be0971f7f9db5efaf7577a0f31796093ddb
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-02-08 10:52:56 -0500

    Make grid tiles array private
    I can't think of any reason that code outside the grid module would need
    to directly access the tiles.  This should help enforce modularity.

--- js/grid.js
+++ js/grid.js
@@ -2,19 +2,18 @@ var Cursor = require('./cursor');
 
 var Grid = function (options) {
     var self = this,
-        cursor = new Cursor(options.cursor);
-
-    this.tiles = []; // an array of tile objects
+        cursor = new Cursor(options.cursor),
+        tiles = []; // an array of tile objects
 
     function createTileElement(row, col) {
-        var tile = self.tiles[row][col];
+        var tile = tiles[row][col];
 
         return $('<div class="grid-tile water">' + tile.value + '</div>')
             .on('click', function () {
                 // make sure the clicked tile is valid
                 if (options.clickTile.validate(cursor, {row: row, col: col, value: tile.value})) {
                     console.log('The click was valid... attempting to move cursor');
-                    cursor.move(row, col, tile.value, self.tiles);
+                    cursor.move(row, col, tile.value, tiles);
                 }
             });
     }
@@ -24,11 +23,11 @@ var Grid = function (options) {
 
         for (i = 0; i < template.height; i++) {
             // push a nested array so we can access tiles like this: [2][1]
-            self.tiles.push([]);
+            tiles.push([]);
 
             // a reference to the DOM element for this row
             for (j = 0; j < template.width; j++) {
-                self.tiles[i].push({
+                tiles[i].push({
                     visited: false,
                     value: Math.ceil(Math.random() * ((Math.sqrt(template.height * template.width)) * 0.6))
                 });
@@ -37,7 +36,7 @@ var Grid = function (options) {
 
         // loop through each predefined tile and create it
         template.definedTiles.forEach(function(tile) {
-            self.tiles[tile.row][tile.col] = tile;
+            tiles[tile.row][tile.col] = tile;
         });
 
         return self;
@@ -46,18 +45,18 @@ var Grid = function (options) {
     this.draw = function ($parent) {
         var i, j, $row,
         $grid = $('<div class="grid" />'),
-        rowCount = self.tiles.length,
-        colCount = self.tiles[0].length;
+        rowCount = tiles.length,
+        colCount = tiles[0].length;
 
         for (i = 0; i < rowCount; i++) {
             $row = $('<div class="grid-row" />').appendTo($grid);
             for (j = 0; j < colCount; j++) {
-                self.tiles[i][j].$el = createTileElement(i, j).appendTo($row);
+                tiles[i][j].$el = createTileElement(i, j).appendTo($row);
             }
         }
 
         // draw the cursor accordion to the options passed to the grid constructor
-        cursor.place(options.cursor, self.tiles[options.cursor.row][options.cursor.col]);
+        cursor.place(options.cursor, tiles[options.cursor.row][options.cursor.col]);
 
         $grid.appendTo($parent || $('body'));
 
