c516f4cca00f48da2d75a861e1009f3a011d2a04
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-02-07 20:01:48 -0500

    Add a draw function to the grid
    We should now be able to call `.draw()` after we run the `construct`
    function.  The function accepts an optional parent jQuery object to
    append to. Otherwise it should append to the body.

--- js/grid.js
+++ js/grid.js
@@ -1,24 +1,42 @@
 var Grid = function () {
-    var self = this;
+    var self = this,
+        $grid = $('<div class="grid" />');
 
     this.tiles = []; // an array of tile objects
 
+    function makeTile(i, j) {
+        return $('<div class="grid-tile water">' + self.tiles[i][j].value + '</div>')
+            .on('click', function () {
+                alert('You clicked the tile at index: ' + i + ' ' + j + ' with a value of:' + self.tiles[i][j].value);
+            })
+            .appendTo($row);
+    }
+
     this.construct = function (height, width) {
-        var i, j;
+        var i, j, $row;
 
         for (i = 0; i < height; i++) {
             // push a nested array so we can access tiles like this: [2][1]
             self.tiles.push([]);
+
+            // a reference to the DOM element for this row
+            $row = $('<div class="grid-row" />').appendTo($grid);
 
             for (j = 0; j < width; j++) {
                 this.tiles[i].push({
                     visited: false,
                     value: Math.ceil(Math.random() * 9)
                 });
+                this.tiles[i][j].$el = makeTile(i, j);
             }
         }
         return self;
-    }
+    };
+
+    this.draw = function ($parent) {
+        $grid.appendTo($parent || $('body'));
+        return self;
+    };
 };
 
 module.exports = Grid;
