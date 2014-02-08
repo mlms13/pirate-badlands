941c69a431f1f00970467c61fa63759b72b5fe3d
Jason <jcvalle@mail.usf.edu>
2014-02-07 21:32:53 -0500

    Connect storage module and check for saved game on load

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 var game = require('./game');
+var storage = require('./storage');
+
+if (localStorage && storage.getUser()) {
+	var user = storage.getUser();
+	console.log(user);
+} else {
+	storage.saveUserState({hasGame: true, noLevels: 0});
+	console.log("saved user state");
+}
 
 $('#startGame').on('click', function() {
 	game.startGame();
