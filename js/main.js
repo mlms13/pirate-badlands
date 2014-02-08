c8ee6b36ce90df6715b32914ff858c6c7a0410f8
stezu <stephen.zuniga001@gmail.com>
2014-02-07 23:27:08 -0500

    Add dynamic height to tiles

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ $('#startGame').on('click', function() {
 	game.startGame();
 	$('document').trigger('gameStarted');
 });
+
+// Handle height for tiles
+$(document).on('gameStarted', function () {
+    getHeight();
+});
+$(window).on('resize', function () {
+    getHeight();
+});
+function getHeight() {
+    var elWidth = $('.grid-tile').first().width();
+
+    if ( $('#inlineHeight').length === 0 ) {
+        $('head').append('<style id="inlineHeight">.grid-tile { height:' + elWidth + 'px }</style>');
+    } else {
+        $('#inlineHeight').html('.grid-tile { height:' + elWidth + 'px }</style>');
+    }
+}
