2c1b23cfd7adb98d0544f314bae16ec2f9645a08
Jason <jcvalle@mail.usf.edu>
2014-02-07 21:32:28 -0500

    Add storage module

---
+++ js/storage.js
@@ -0,0 +1,11 @@
+var storage = {};
+
+storage.getUser = function () {
+	return JSON.parse(localStorage.getItem('user'));
+}
+
+storage.saveUserState = function (user) {
+	localStorage.setItem('user', JSON.stringify(user));
+}
+
+module.exports = storage;
