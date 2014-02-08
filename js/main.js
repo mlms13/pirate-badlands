db5100e25743afa307c9bb5bcd36589b05d77ec0
Jason <jcvalle@mail.usf.edu>
2014-02-07 21:55:19 -0500

    remove logs

--- js/main.js
+++ js/main.js
@@ -3,10 +3,8 @@ var storage = require('./storage');
 
 if (localStorage && storage.getUser()) {
 	var user = storage.getUser();
-	console.log(user);
 } else {
 	storage.saveUserState({noLevels: 0, achievements: []});
-	console.log("saved user state");
 }
 
 $('#startGame').on('click', function() {
