ebd62b3059a8f956628e2a17a416a3cd5118d962
stezu <stephen.zuniga001@gmail.com>
2014-02-09 10:25:02 -0500

    Remove visited from templates
    It’s already defined in our original loop.

--- js/templates.js
+++ js/templates.js
@@ -4,12 +4,12 @@ var templates = [
         width: 6,
         startPos: {row: 4, col: 1},
         definedTiles: [
-            {row: 4, col: 2, visited: false, value: 3},
-            {row: 3, col: 4, visited: false, value: 3},
-            {row: 0, col: 4, visited: false, value: 3},
-            {row: 1, col: 5, visited: false, value: 1},
-            {row: 1, col: 4, visited: false, value: 1, points: 50},
-            {row: 0, col: 5, visited: false, value: 1, endGame: true}
+            {row: 4, col: 2, value: 3},
+            {row: 3, col: 4, value: 3},
+            {row: 0, col: 4, value: 3},
+            {row: 1, col: 5, value: 1},
+            {row: 1, col: 4, value: 1, points: 50},
+            {row: 0, col: 5, value: 1, endGame: true}
         ]
     },
     {
@@ -17,16 +17,16 @@ var templates = [
         width: 6,
         startPos: {row: 4, col: 0},
         definedTiles: [
-            {row: 1, col: 1, visited: false, value: 1},
-            {row: 3, col: 1, visited: false, value: 4},
-            {row: 4, col: 1, visited: false, value: 1},
-            {row: 5, col: 1, visited: false, value: 3},
-            {row: 0, col: 2, visited: false, value: 3},
-            {row: 1, col: 2, visited: false, value: 4},
-            {row: 4, col: 2, visited: false, value: 6},
-            {row: 2, col: 3, visited: false, value: 3},
-            {row: 1, col: 4, visited: false, value: 2},
-            {row: 1, col: 5, visited: false, value: 1, endGame: true}
+            {row: 1, col: 1, value: 1},
+            {row: 3, col: 1, value: 4},
+            {row: 4, col: 1, value: 1},
+            {row: 5, col: 1, value: 3},
+            {row: 0, col: 2, value: 3},
+            {row: 1, col: 2, value: 4},
+            {row: 4, col: 2, value: 6},
+            {row: 2, col: 3, value: 3},
+            {row: 1, col: 4, value: 2},
+            {row: 1, col: 5, value: 1, endGame: true}
         ]
     },
     {
@@ -34,19 +34,19 @@ var templates = [
         width: 7,
         startPos: {row: 3, col: 3},
         definedTiles: [
-            {row: 1, col: 0, visited: false, value: 2},
-            {row: 5, col: 0, visited: false, value: 6},
-            {row: 0, col: 1, visited: false, value: 2},
-            {row: 3, col: 1, visited: false, value: 3},
-            {row: 1, col: 2, visited: false, value: 4},
-            {row: 3, col: 2, visited: false, value: 6},
-            {row: 4, col: 2, visited: false, value: 2},
-            {row: 2, col: 3, visited: false, value: 6},
-            {row: 4, col: 3, visited: false, value: 1},
-            {row: 3, col: 4, visited: false, value: 6},
-            {row: 6, col: 4, visited: false, value: 5},
-            {row: 2, col: 5, visited: false, value: 5},
-            {row: 0, col: 0, visited: false, value: 1, endGame: true}
+            {row: 1, col: 0, value: 2},
+            {row: 5, col: 0, value: 6},
+            {row: 0, col: 1, value: 2},
+            {row: 3, col: 1, value: 3},
+            {row: 1, col: 2, value: 4},
+            {row: 3, col: 2, value: 6},
+            {row: 4, col: 2, value: 2},
+            {row: 2, col: 3, value: 6},
+            {row: 4, col: 3, value: 1},
+            {row: 3, col: 4, value: 6},
+            {row: 6, col: 4, value: 5},
+            {row: 2, col: 5, value: 5},
+            {row: 0, col: 0, value: 1, endGame: true}
         ]
     },
     {
@@ -54,21 +54,21 @@ var templates = [
         width: 8,
         startPos: {row: 0, col: 0},
         definedTiles: [
-            {row: 1, col: 0, visited: false, value: 6},
-            {row: 5, col: 1, visited: false, value: 5},
-            {row: 6, col: 1, visited: false, value: 1},
-            {row: 7, col: 1, visited: false, value: 4},
-            {row: 1, col: 2, visited: false, value: 4},
-            {row: 6, col: 2, visited: false, value: 8},
-            {row: 7, col: 4, visited: false, value: 6},
-            {row: 2, col: 5, visited: false, value: 2},
-            {row: 4, col: 5, visited: false, value: 4},
-            {row: 3, col: 6, visited: false, value: 1},
-            {row: 4, col: 6, visited: false, value: 3},
-            {row: 7, col: 6, visited: false, value: 6},
-            {row: 6, col: 7, visited: false, value: 1, endGame: true}
+            {row: 1, col: 0, value: 6},
+            {row: 5, col: 1, value: 5},
+            {row: 6, col: 1, value: 1},
+            {row: 7, col: 1, value: 4},
+            {row: 1, col: 2, value: 4},
+            {row: 6, col: 2, value: 8},
+            {row: 7, col: 4, value: 6},
+            {row: 2, col: 5, value: 2},
+            {row: 4, col: 5, value: 4},
+            {row: 3, col: 6, value: 1},
+            {row: 4, col: 6, value: 3},
+            {row: 7, col: 6, value: 6},
+            {row: 6, col: 7, value: 1, endGame: true}
         ]
-    },    
+    },
 ];
 
 module.exports = templates;
